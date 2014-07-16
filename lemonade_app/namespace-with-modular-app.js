48ce058e6e9157681c834ed9672a790ffff91a97
Prabhakar <pdoraisamy@vmware.com>
2014-07-16 16:29:20 -0700

    App with module dependency

--- lemonade_app/namespace-with-modular-app.js
+++ lemonade_app/namespace-with-modular-app.js
@@ -4,36 +4,53 @@ This snippet is to demo few basic concepts:
 - Self-executing function
 - Module pattern
 - Taking window object into a module as dependency module.
-- private property & public methods, and how to expose methods outside the module
+- private property & public methods, and how to expose methods outside the module.
+- A sample module injected into the LEMON app as dependency.
 */
-
 'use strict';
-
 var LEMON = LEMON || {};
-
 LEMON.Marketting = LEMON.Marketting || {};
 
-LEMON.Marketting = (function(w) {
+LEMON.Marketting = (function(w, transactionModule, $) {
 
    var promptPopup = function() {
-      var n = prompt('Enter your favorite pick', '');
-      console.log(n);
+      console.log('first one');
+   };
+
+   var refreshView = function() {
+      var str = "";
+      for (var i = 0; i < transactionModule.items.length; i++) {
+         str += "<li id='" + transactionModule.items[i].id + "'>" + transactionModule.items[i].name + " | " + transactionModule.items[i].quantity + "</li>";
+      }
+      $('.content ul').html(str);
+      $('#cart').html('<span>Total cost: ' + transactionModule.getTotalCost() + '</span>');
+   }
+
+   var init = function() {
+      refreshView();
+      $('.content ul').on('click', '>li', function(e) {
+         var id;
+         console.log(e.target);
+         id = $(e.target).attr('id');
+         transactionModule.setItemCost(id);
+         refreshView();
+      });
    };
 
    return {
-      promptFavoritePick: promptPopup
+      promptFavoritePick: promptPopup,
+      init: init
    }
 
-}(window));
+}(window, transactionModule, $));
 
-LEMON.Marketting.promptFavoritePick();
+LEMON.Marketting.init();
 
 //This is interesting because the module methods can still be overwritten eventhough it looks like its carefully boxed inside.
 //Uncomment below code to understand.
-/*
-LEMON.Marketting.promptFavoritePick = function() {
-   console.log('second one');
-}
 
-LEMON.Marketting.promptFavoritePick();
-*/
+// LEMON.Marketting.promptFavoritePick = function() {
+//    console.log('second one');
+// }
+
+// LEMON.Marketting.promptFavoritePick();
---
+++ lemonade_app/transactionModule.js
@@ -0,0 +1,42 @@
+transactionModule = (function() {
+
+   var items = [{
+      name: 'Medium Lemonade',
+      id: 'mL',
+      cost: 10,
+      quantity: 0,
+   }, {
+      name: 'Large Lemonade',
+      id: 'lL',
+      cost: 5,
+      quantity: 0
+   }, {
+      name: 'healthy Snack',
+      id: 'hS',
+      cost: 5,
+      quantity: 0
+   }];
+
+   var setItemCost = function(item) {
+      for (var i = 0; i < items.length; i++) {
+         if (items[i].id === item) {
+            items[i].quantity++;
+         }
+      }
+   };
+
+   var getTotalCost = function() {
+      var totalCost = 0;
+      for (var i = 0; i < items.length; i++) {
+         totalCost = totalCost + (items[i].quantity * items[i].cost);
+      }
+      return totalCost;
+   };
+
+   return {
+      setItemCost: setItemCost,
+      items: items,
+      getTotalCost: getTotalCost
+   }
+
+}());
